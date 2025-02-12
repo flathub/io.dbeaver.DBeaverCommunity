539613dcd548dbae85b04f46ef0ebabe156a8d27
DodoLeDev <59477313+DodoLeDev@users.noreply.github.com>
2025-02-12 19:23:48 +0100

    feat: Added migration tool

--- dbeaver.sh
+++ dbeaver.sh
@@ -1,2 +1,16 @@
 #!/usr/bin/env bash
-exec /app/bin/dbeaver -data "$XDG_DATA_HOME/workspace"
+
+WORKSPACE_FOLDER="$XDG_DATA_HOME/workspace"
+DRIVERS_FOLDER="$XDG_DATA_HOME/drivers"
+
+# If workspace data is still in old place, start the migration process
+if [ -d "$HOME/.local/share/DBeaverData/workspace6" ]
+then
+  mkdir "$WORKSPACE_FOLDER" "$DRIVERS_FOLDER"
+  mv "$HOME"/.local/share/DBeaverData/workspace6/* "$HOME"/.local/share/DBeaverData/workspace6/.* "$WORKSPACE_FOLDER"
+  mv "$HOME"/.local/share/DBeaverData/drivers/* "$DRIVERS_FOLDER"
+  rmdir "$HOME"/.local/share/DBeaverData/workspace6 "$HOME"/.local/share/DBeaverData/drivers/
+  /app/bin/migration-dialog
+fi
+
+exec /app/bin/dbeaver -data "$WORKSPACE_FOLDER"
