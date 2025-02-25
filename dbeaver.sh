9d9d263cc2b5187322dd71f479cc4ff36f611571
DodoLeDev <59477313+DodoLeDev@users.noreply.github.com>
2025-02-26 00:52:17 +0100

    fix: Fixed migration script

--- dbeaver.sh
+++ dbeaver.sh
@@ -6,7 +6,6 @@ DRIVERS_FOLDER="$XDG_DATA_HOME/drivers"
 # If workspace data is still in old place, start the migration process
 if [ -d "$HOME/.local/share/DBeaverData/workspace6" ] && [ ! -d "$WORKSPACE_FOLDER" ]
 then
-  mkdir "$WORKSPACE_FOLDER" "$DRIVERS_FOLDER"
   cp -r "$HOME"/.local/share/DBeaverData/workspace6 "$WORKSPACE_FOLDER"
   cp -r "$HOME"/.local/share/DBeaverData/drivers "$DRIVERS_FOLDER"
   /app/bin/migration-dialog
