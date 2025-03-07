0650861c6efb2da7bad0746a95c23b4d0e246709
Eonfge <33983090+Eonfge@users.noreply.github.com>
2025-03-07 13:40:36 +0000

    Improved message

--- dbeaver.sh
+++ dbeaver.sh
@@ -6,7 +6,7 @@ DRIVERS_FOLDER="$XDG_DATA_HOME/drivers"
 
 # New users are informed of the improved sandboxing options.
 # We might remove this notice in the future, although this is still open for debate.
-if [ ! -d "$WORKSPACE_FOLDER" ]
+if [ ! -d "$HOME/.local/share/DBeaverData/workspace6" ] && [ ! -d "$WORKSPACE_FOLDER" ]
 then
   cp -r "$HOME"/.local/share/DBeaverData/workspace6 "$WORKSPACE_FOLDER"
   cp -r "$HOME"/.local/share/DBeaverData/drivers "$DRIVERS_FOLDER"
@@ -21,7 +21,7 @@ then
   cp -r "$HOME"/.local/share/DBeaverData/workspace6 "$WORKSPACE_FOLDER"
   cp -r "$HOME"/.local/share/DBeaverData/drivers "$DRIVERS_FOLDER"
 
-  zenity --info --title="DBeaver improved sandboxing" --text="The default location for storing DBeaver's files has changed. They are now stored in the \"/.var/app/io.dbeaver.DBeaverCommunity\" folder. \n\nIf you want to, you can now improve DBeaver' sandboxing by remove Home-access. To do so, you can use an application like Flatseal."
+  zenity --info --title="DBeaver improved sandboxing" --text="The default location for storing DBeaver's files has changed. They are now stored in the <tt>/.var/app/io.dbeaver.DBeaverCommunity</tt> folder. \n\nIf you want to, you can now improve DBeaver' sandboxing by remove Home-access. To do so, you can use an application like Flatseal."
 fi
 
 exec /app/bin/dbeaver -data "$WORKSPACE_FOLDER" -configuration "$CONFIG_FOLDER" "$@"
