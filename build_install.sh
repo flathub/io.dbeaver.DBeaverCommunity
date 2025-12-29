324b3466b4df9aa8e091ccee995a3a1b702bed19
Wistara Banyu Kayana <wisthara.banyu.kayana@gmail.com>
2025-12-29 20:38:14 +0700

    Updated build_install.sh and README.md

--- build_install.sh
+++ build_install.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 clear
+
+#Update Flatpak SDKs and Platforms According to Manifest Requirements
+flatpak install flathub org.gnome.Sdk//49 -y
+flatpak install flathub org.gnome.Platform//49 -y
+flatpak install flathub org.freedesktop.Sdk.Extension.openjdk//25.08 -y
+
 flatpak-builder --repo=testing-repo --force-clean build-dir io.dbeaver.DBeaverCommunity.yml
 flatpak --user remote-add --if-not-exists --no-gpg-verify db-testing-repo testing-repo
 flatpak --user install db-testing-repo io.dbeaver.DBeaverCommunity -y
